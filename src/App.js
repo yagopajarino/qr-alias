9ace873d44931255727b0545393067370c176736
Yago Pajariño <yagopajarino@outlook.com>
2023-01-16 21:26:24 -0300

    Add bones components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-function App() {
+import Header from "./components/Header.js";
+import Main from "./components/Main.js";
+import Donaciones from "./components/Donaciones.js";
+import Footer from "./components/Footer.js";
+import React from "react";
+
+export default function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello World</h1>
+      <Header />
+      <Main />
+      <Donaciones />
+      <Footer />
     </div>
   );
 }
-
-export default App;
---
+++ src/components/Donaciones.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Donaciones() {
+  return (
+    <div>
+      <h1>Donaciones</h1>
+    </div>
+  );
+}
